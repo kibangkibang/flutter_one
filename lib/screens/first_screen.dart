3ec6e9ef26ae58c124bc8150d185b8dcd8c1b05a
kibangkibang <csh0989@naver.com>
2022-06-03 00:10:37 +0900

    Ex_State

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,27 +1,43 @@
-import 'package:chapter_one/screens/second_screen.dart';
 import 'package:flutter/material.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  int count = 0;
+
+  void increase(){
+    setState(() {
+      count = count + 1;
+    });
+  }
+
+  void decrease(){
+    setState(() {
+      count = count - 1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("카운터 앱"),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("This is First Screen"),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          SecondScreen(screenData: 'Data from First Screen'),
-                    ),
-                  );
-                },
-                child: Text("Go to Second Screen"))
+            Text("카운트: $count", style: TextStyle(fontSize: 25)),
+            Padding(padding: EdgeInsets.all(20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: decrease, child: Text("- 감소")),
+                ElevatedButton(onPressed: increase, child: Text("+ 증가")),
+              ],
+            )
           ],
         ),
       ),
